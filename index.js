f741a3dba6482973017545dc1a827c7a8464fc78
Marthe <66720530+martheberget@users.noreply.github.com>
2023-01-09 18:20:22 +0100

    Restructured code

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    console.log("Tick");
-  };
-}
-
 const durationInput = document.querySelector(".duration");
 const startButton = document.querySelector(".start");
 const pauseButton = document.querySelector(".pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+// Separating the timer from the border
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("On tick");
+  },
+  onComplete() {
+    console.log("On complete");
+  },
+});
