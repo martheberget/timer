ca323540383fc522dd54823433890cd647a80cfe
Marthe <66720530+martheberget@users.noreply.github.com>
2023-01-12 17:50:39 +0100

    More functionality and added styling

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const durationInput = document.querySelector(".duration");
 const startButton = document.querySelector(".start");
 const pauseButton = document.querySelector(".pause");
 
+const circle = document.querySelector(".circle");
+
+const perimeter = 2 * Math.PI * circle.getAttribute("r");
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let duration;
+
 // Separating the timer from the border
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log("Timer started");
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    console.log("On tick");
+  onTick(timeRemaining) {
+    offset = (perimeter * timeRemaining) / duration - perimeter;
+    circle.setAttribute("stroke-dashoffset", offset);
   },
   onComplete() {
     console.log("On complete");
--- timer.js
+++ timer.js
@@ -15,10 +15,10 @@ class Timer {
 
   start = () => {
     if (this.onStart) {
-      this.onStart();
+      this.onStart(this.timeRemaining);
     }
     this.tick();
-    this.interval = setInterval(this.tick, 1000);
+    this.interval = setInterval(this.tick, 20);
   };
 
   pause = () => {
@@ -32,9 +32,9 @@ class Timer {
         this.onComplete();
       }
     } else {
-      this.timeRemaining = this.timeRemaining - 1;
+      this.timeRemaining = this.timeRemaining - 0.02;
       if (this.onTick) {
-        this.onTick();
+        this.onTick(this.timeRemaining);
       }
     }
   };
@@ -44,6 +44,6 @@ class Timer {
   }
 
   set timeRemaining(time) {
-    this.durationInput.value = time;
+    this.durationInput.value = time.toFixed(2);
   }
 }
